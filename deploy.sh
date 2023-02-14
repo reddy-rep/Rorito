545b3d47ed3d795555c81077c842ca51ae2ed3b8
ReddaiahNaidu <reddaiah.naidurhel@gmail.com>
2023-02-14 16:51:03 +0000

    Adding deployment script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+Mount a drive to the remote machine (where the shell script is located) on the machine hosting the Informatica Cloud Secure Agent.
+The shell script will now be accessible to the agent using standard path syntax (example: /remote/usr/local/).
+
+In the Command Task enter the entire path and the shell script name.
+
+Example
+/remote/usr/local/test.sh
